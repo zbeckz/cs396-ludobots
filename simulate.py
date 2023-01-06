84a530b302227c37196f526489e5a5778ec1205b
zbeckz <zach@thebeckers.cc>
2023-01-06 12:50:10 -0600

    floor

--- simulate.py
+++ simulate.py
@@ -1,13 +1,17 @@
 import pybullet as p
+import pybullet_data
 import time
 
+# setup
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 # load world
-p.setGravity(0,0,-9.8)
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)                  # gravity
+planeId = p.loadURDF("plane.urdf")      # floor
+p.loadSDF("box.sdf")                    # world with just a box in it
 
-# show the world for some time
+# simulate the world
 for i in range(10000):
     print(i)    
     p.stepSimulation()
