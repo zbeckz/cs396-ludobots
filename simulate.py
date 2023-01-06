fe13dbc7713cda38420109da947f56671d43b2ac
zbeckz <zach@thebeckers.cc>
2023-01-06 12:33:42 -0600

    box in world

--- simulate.py
+++ simulate.py
@@ -3,7 +3,11 @@ import time
 
 physicsClient = p.connect(p.GUI)
 
-for i in range(1000):
+# load box into the world
+p.loadSDF("box.sdf")
+
+# show the world for some time
+for i in range(10000):
     print(i)    
     p.stepSimulation()
     time.sleep(1/60)
