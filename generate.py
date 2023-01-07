d0a11ec4ef10fdda1f246730a19ae3c3ee85f946
zbeckz <zach@thebeckers.cc>
2023-01-06 18:29:51 -0600

    new world for part E

--- generate.py
+++ generate.py
@@ -1,23 +1,6 @@
 import pyrosim.pyrosim as pyrosim
 
-# makes a tower of 10 boxes, with each higher box being 90% the size of the one below it, with base at (x, y, 0)
-def makeTower(x, y):
-    for i in range(10):
-        z = 0.5 + i             # each block placed on top of one before it
-        length = 1 * 0.9**i     # each block 90% size of one before it
-        width = 1 * 0.9**i
-        height = 1 * 0.9**i
-        pyrosim.Send_Cube(name="Box " + str(i+1), pos=[x,y,z] , size=[length,width,height])
+pyrosim.Start_SDF("world.sdf")
+pyrosim.Send_Cube(name="Box ", pos=[0, 0, 0.5] , size=[1,1,1])
+pyrosim.End()   
 
-def main():
-    pyrosim.Start_SDF("boxes.sdf")
-
-    # generate a 5 by 5 grid of towers
-    for i in range(-2, 3):
-        for j in range(-2, 3):
-            makeTower(i, j)
-
-    pyrosim.End()    
-
-if __name__ == "__main__":
-    main()
--- simulate.py
+++ simulate.py
@@ -9,7 +9,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # load world
 p.setGravity(0,0,-9.8)                  # gravity
 planeId = p.loadURDF("plane.urdf")      # floor
-p.loadSDF("boxes.sdf")                  # world with just a box in it
+p.loadSDF("world.sdf")                  # world with just a box in it
 
 # simulate the world
 for i in range(10000):
