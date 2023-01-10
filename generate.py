2f988d4aed5e27fc12391a6577ac676412d5102a
zbeckz <zach@thebeckers.cc>
2023-01-10 17:52:19 -0600

    random weights

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -28,10 +29,9 @@ def Generate_Brain():
     pyrosim.Send_Motor_Neuron(name = 3, jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name = 4, jointName = "Torso_FrontLeg")
     
-    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 3, weight = 0.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 3, weight = 0.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 4, weight = 0.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 4, weight = -1.0)
+    for i in range(3):
+        for j in range(3, 5):
+            pyrosim.Send_Synapse(sourceNeuronName = i, targetNeuronName = j, weight = random.random()*2 - 1)
     
     pyrosim.End()
 
