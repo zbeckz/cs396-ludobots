c295c3fb749e3af93a96f2b109f8268b6e83533e
zbeckz <zach@thebeckers.cc>
2023-01-14 12:50:28 -0600

    changed code structure to statically create world and body

--- solution.py
+++ solution.py
@@ -3,12 +3,13 @@ import pyrosim.pyrosim as pyrosim
 import os
 import random
 import time
+import constants as c
 
 class SOLUTION:
 
     def __init__(self, id):
         self.myID = id
-        self.weights = np.random.rand(3, 2) * 2 - 1
+        self.weights = np.random.rand(c.numSensorNeurons, c.numMotorNeurons) * 2 - 1
 
     def Set_ID(self, id):
         self.myID = id
@@ -37,20 +38,6 @@ class SOLUTION:
     def Mutate(self):
         self.weights[random.randint(0, 2)][random.randint(0, 1)] = random.random()*2 - 1
 
-    def Create_World(self):
-        pyrosim.Start_SDF("world.sdf")
-        pyrosim.Send_Cube(name="Box", pos=[-5, 5, 0.5] , size=[1,1,1])
-        pyrosim.End()   
-
-    def Create_Body(self):
-        pyrosim.Start_URDF("body.urdf")
-        pyrosim.Send_Cube(name= "Torso", pos=[1.5, 0, 1.5] , size=[1,1,1])
-        pyrosim.Send_Joint(name = "Torso_BackLeg", parent= "Torso", child = "BackLeg", type = "revolute", position = [1,0,1])
-        pyrosim.Send_Cube(name= "BackLeg", pos=[-0.5, 0, -0.5] , size=[1,1,1])
-        pyrosim.Send_Joint(name = "Torso_FrontLeg", parent= "Torso", child = "FrontLeg", type = "revolute", position = [2,0,1])
-        pyrosim.Send_Cube(name= "FrontLeg", pos=[0.5, 0, -0.5] , size=[1,1,1])
-        pyrosim.End()
-
     def Create_Brain(self):
         pyrosim.Start_NeuralNetwork(f"brain{self.myID}.nndf")
         
@@ -61,10 +48,24 @@ class SOLUTION:
         pyrosim.Send_Motor_Neuron(name = 3, jointName = "Torso_BackLeg")
         pyrosim.Send_Motor_Neuron(name = 4, jointName = "Torso_FrontLeg")
     
-        for currentRow in range(3):
-            for currentCol in range(2):
+        for currentRow in range(c.numSensorNeurons):
+            for currentCol in range(c.numMotorNeurons):
                 pyrosim.Send_Synapse(sourceNeuronName = currentRow, 
                                      targetNeuronName = currentCol + 3, 
                                      weight = self.weights[currentRow][currentCol])
     
         pyrosim.End()
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-5, 5, 0.5] , size=[1,1,1])
+    pyrosim.End()   
+
+def Create_Body():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name= "Torso", pos=[0, 0, 1] , size=[1,1,1])
+    pyrosim.Send_Joint(name = "Torso_BackLeg", parent= "Torso", child = "BackLeg", type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name= "BackLeg", pos=[-0.5, 0, -0.5] , size=[1,1,1])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg", parent= "Torso", child = "FrontLeg", type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name= "FrontLeg", pos=[0.5, 0, -0.5] , size=[1,1,1])
+    pyrosim.End()
